3f01e4c58bf68af8f4871186b540e914de8db900
Jyotirmoy Bhattacharya <jyotirmoy@jyotirmoy.net>
2014-12-31 12:43:22 +0530

    No toplevel using directive in header

--- cxx-common/norvig.h
+++ cxx-common/norvig.h
@@ -1,29 +1,28 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
 #include <string>
+#include <cstring>
 #include <cctype>
+#include <algorithm>
 
-using namespace std;
-
-const string alphabet="abcdefghijklmnopqrstuvwxyz";
+const std::string alphabet="abcdefghijklmnopqrstuvwxyz";
 
 /*
   Editing functions. With each edit found they call the
   provided function object 'f'.
 */
-template<class FO> void deletes(const string&word, FO &f)
+template<class FO> void deletes(const std::string&word, FO &f)
 {
   for (size_t n=0;n<word.length();++n){
-    string s=word;
+    std::string s=word;
     s.erase(n,1);
     f(s);
   }
 }
 
-template<class FO> void replaces(const string&word, FO &f)
+template<class FO> void replaces(const std::string&word, FO &f)
 {
-  string s = word;
+  std::string s = word;
   for (size_t n=0;n<word.length();n++){
     char c = s[n];
     for (size_t i=0;i<alphabet.length();i++){
@@ -34,10 +33,10 @@ template<class FO> void replaces(const string&word, FO &f)
   }
 }
 
-template<class FO> void inserts(const string&word, FO &f)
+template<class FO> void inserts(const std::string&word, FO &f)
 {
   auto N = word.length();
-  string s;
+  std::string s;
   
   for (size_t n=0;n<=N;n++){
     s.resize(N+1);
@@ -52,17 +51,17 @@ template<class FO> void inserts(const string&word, FO &f)
   }
 }
 
-template<class FO> void transposes(const string&word, FO &f)
+template<class FO> void transposes(const std::string&word, FO &f)
 {
-  string s = word;
+  std::string s = word;
   for (size_t n=0;n<word.length()-1;n++){
-    swap(s[n],s[n+1]);
+    std::swap(s[n],s[n+1]);
     f(s);
-    swap(s[n],s[n+1]);
+    std::swap(s[n],s[n+1]);
  }
 }
 
-template<class FO> void edits1(const string &word,FO &f)
+template<class FO> void edits1(const std::string &word,FO &f)
 {
   deletes(word,f);
   replaces(word,f);
@@ -79,9 +78,9 @@ template<class Freq> struct MaxState
 {
   Freq &m;
   long max;
-  string maxarg;
+  std::string maxarg;
   MaxState<Freq>(Freq &mod) : m(mod),max(0) {}
-  void operator()(const string &s){
+  void operator()(const std::string &s){
     long val = m.lookup(s);
     if (val>max){
       max = val;
@@ -100,7 +99,7 @@ template<class T> class TwoState
   T &inner;
 public:
   TwoState<T>(T &in) : inner(in) {}
-  void operator()(const string &s) {edits1(s,inner);}
+  void operator()(const std::string &s) {edits1(s,inner);}
 };
 
 /*
@@ -108,19 +107,19 @@ public:
   It is parametrised over the type of frequency table.
   It expects the table to provide two public methods
 
-    void update(const string &s) 
+    void update(const std::string &s) 
 
   which adds 1 to the observed frequency of 's', and
 
-    unsigned long lookup(const string &s)
+    unsigned long lookup(const std::string &s)
 
   which returns the observed frequency of 's'.
 */
 template<class Freq> class Norvig {
   Freq freq;
 public:
-  void train(istream &s);
-  string correct(const string &);
+  void train(std::istream &s);
+  std::string correct(const std::string &);
   int main(int argc,char *argv[]);
 };
 
@@ -129,7 +128,7 @@ public:
   itself if no correction is found.
 */
 template<class F>
-string Norvig<F>::correct(const string &word)
+std::string Norvig<F>::correct(const std::string &word)
 {
   if (freq.lookup(word))
     return word;
@@ -148,8 +147,9 @@ string Norvig<F>::correct(const string &word)
   Train the model based on word frequencies in the given file
 */
 template<class F>
-void Norvig<F>::train(istream &fs)
+void Norvig<F>::train(std::istream &fs)
 {
+  using namespace std;
   while (fs){
     char c;
     string wrd;
@@ -174,6 +174,7 @@ void Norvig<F>::train(istream &fs)
 template<class F>
 int Norvig<F>::main(int argc,char*argv[])
 {
+  using namespace std;
   if (argc!=2){
     cerr<<"Usage: norvig [training dataset]\n";
     return 2;
--- cxx_hat/norvig.cc
+++ cxx_hat/norvig.cc
@@ -1,3 +1,6 @@
+#include <string>
+using namespace std;
+
 #include "hat-trie.h"
 #include "norvig.h"
 
